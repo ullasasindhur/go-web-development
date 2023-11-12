049132bc836465df780d6238cebf13b358665fb3
ullasasindhur <ullasasindhur@gmail.com>
2023-11-12 21:18:33 +0530

    Implemented api functions

--- crud-go-apis/main.go
+++ crud-go-apis/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -25,26 +26,55 @@ func main() {
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
-	if err := http.ListenAndServe(":8080", r); err != nil {
-		fmt.Println("crud go apis server is started at port 8080")
-	} else {
-		log.Fatal(err)
-	}
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
 func getMovies(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
 }
 
 func getMovie(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, movie := range movies {
+		if movie.id == params["id"] {
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
 }
 func createMovie(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var newMovie movie
+	json.NewDecoder(r.Body).Decode(&newMovie)
+	movies = append(movies, newMovie)
+	json.NewEncoder(w).Encode(movies)
 }
 
 func updateMovie(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, movie := range movies {
+		if movie.id == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	var updateMovie movie
+	json.NewDecoder(r.Body).Decode(&updateMovie)
+	movies = append(movies, updateMovie)
+	json.NewEncoder(w).Encode(movies)
 }
 
-func deleteMovie(w http.ResponseWriter, r *http.Request)
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, movie := range movies {
+		if movie.id == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
